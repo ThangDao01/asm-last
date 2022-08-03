6c743a8f3fcfd1439a0895aa4a85eecc8ff0810d
ThangDao01 <thangddth2009001@fpt.edu.vn>
2022-08-03 17:23:12 +0700

    first commit

---
+++ src/main/java/com/example/demo/service/StockService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.Stock;
+import com.example.demo.repository.StockRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StockService {
+    @Autowired
+    StockRepo stockRepo;
+
+    public List<Stock> getAll(){
+        return stockRepo.findAll();
+    }
+
+    public List<Stock> getStockName(String symbol) {
+        return stockRepo.searchName(symbol);
+    }
+
+    public List<Double> getStockPrice(String symbol) {
+        return stockRepo.getStockPrice(symbol);
+    }
+}
